093a17aed591e15721554fa8f508e8dbd66f4dc4
Pubadee Klinocha <poom90914@gmail.com>
2021-11-05 03:48:12 +0700

    modify cloudfront

--- modules/s3-webhosting/cloudfront.tf
+++ modules/s3-webhosting/cloudfront.tf
@@ -86,8 +86,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     restrictions {
       geo_restriction {
-        restriction_type = "whitelist"
-        locations        = ["US", "CA", "GB", "DE"]
+        restriction_type = "none"
+        # locations        = ["US", "CA", "GB", "DE"]
       }
     }
 
